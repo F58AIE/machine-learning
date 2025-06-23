22789ce869a31554041380084408349c5ca76ac3
f58dev <192527421+f58dev@users.noreply.github.com>
2025-06-24 00:34:41 +0300

    Msg

--- main.py
+++ main.py
@@ -1,43 +1,43 @@
-# # # first_name = "ahmed"
-# # # last_name = "Mohd"
-# # # print("My name is: " +first_name + " " + last_name)
+# first_name = "ahmed"
+# last_name = "Mohd"
+# print("My name is: " +first_name + " " + last_name)
 
-# # # # string "I love Python"
-# # # wht = "I love Python" 
-# # # print(wht);
-# # # # integer 201
-# # # ints = 201
-# # # print(int)
-# # # # float 99.9
-# # # floats = 99.9
-# # # print(float)
-# # # # boolean true or false
-# # # bools = True
-# # # print(bool)
-
-
-# # name = "F58AIE"
-# # age = 30
-# # score = 99.9
-# # engineer = True
-# # print("My name is:" + name  + " and my age is: " + str(age) + " and my score is: " + str(score) + " and I am an engineer: " + str(engineer))
+# # string "I love Python"
+# wht = "I love Python" 
+# print(wht);
+# # integer 201
+# ints = 201
+# print(int)
+# # float 99.9
+# floats = 99.9
+# print(float)
+# # boolean true or false
+# bools = True
+# print(bool)
 
 
-# # # * / + - 
-# # num1 = 12
-# # num2 = 93
-# # print(num1 + num2)
-# # print(num1 * num2)
-# # print(num1 / num2)
-# # print(num1 - num2) 
-# # print(num1 ** num2)  # Exponentiation operation
-# # num5 = 15
-# # num4 = "432"
-# # print(num5 + int(num4))  # Convert string to integer before addition
-# # print(num5 * int(num4))  # Convert string to integer before multiplication
-# # print(num5 / int(num4))  # Convert string to integer before division
-# # print(num5 - int(num4))  # Convert string to integer before subtraction 
-# # print(num5 ** int(num4))  # Convert string to integer before exponentiation
+# name = "F58AIE"
+# age = 30
+# score = 99.9
+# engineer = True
+# print("My name is:" + name  + " and my age is: " + str(age) + " and my score is: " + str(score) + " and I am an engineer: " + str(engineer))
+
+
+# # * / + - 
+# num1 = 12
+# num2 = 93
+# print(num1 + num2)
+# print(num1 * num2)
+# print(num1 / num2)
+# print(num1 - num2) 
+# print(num1 ** num2)  # Exponentiation operation
+# num5 = 15
+# num4 = "432"
+# print(num5 + int(num4))  # Convert string to integer before addition
+# print(num5 * int(num4))  # Convert string to integer before multiplication
+# print(num5 / int(num4))  # Convert string to integer before division
+# print(num5 - int(num4))  # Convert string to integer before subtraction 
+# print(num5 ** int(num4))  # Convert string to integer before exponentiation
 
 # name = input ("What is your name? ")
 # age = input("what is your age? ")
@@ -49,6 +49,7 @@
 # print(int(num1) + int(num2)) # Convert
 
 
-for x in range(1, 101):
+for x in range(0, 101 , 2):
     print("Hello World " + str(x))
 # for x in range from 0 to 100 because the range function starts from 0 by default
+# The step value of 2 means it will print every second number (even numbers only).
