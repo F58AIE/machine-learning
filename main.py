b03154eadae220428d4676c77581b63f0e107e3a
f58dev <192527421+f58dev@users.noreply.github.com>
2025-06-23 22:04:49 +0300

    hello

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ last_name = "Mohd"
 print("My name is: " +first_name + " " + last_name)
 
 # string "I love Python"
+str = "I love Python" 
+print(str);
 # integer 201
-# float 99.9 
+int = 201
+print(int)
+# float 99.9
+float = 99.9
+print(float)
 # boolean true or false
+bool = True
+print(bool)
