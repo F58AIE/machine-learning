8f7b74087b28d8999d8d24bf895fdb19054617c1
f58dev <192527421+f58dev@users.noreply.github.com>
2025-06-24 22:15:42 +0300

    pandas

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import pandas as pd
 
-import csv 
-with open('big_students.csv', 'r') as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        print(row)      
+data = pd.read_csv('big_students.csv')
+print(data.head())
+
+
+# print(len(data)) to know how many rows are in the dataset
+# print(data['age']) to know how the ages for each student
+# print(data['age'].mean())  # Average age of students
+# print(data['age'].max())   # Maximum age of students
+# print(data['age'].min())   # Minimum age of students    
