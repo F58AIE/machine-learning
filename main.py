b9491da3891b00e6170ecc0903f558e23482c73d
f58dev <192527421+f58dev@users.noreply.github.com>
2025-06-24 22:59:47 +0300

    Data Analysis + Visualization

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 # pandas = excel inside python
-#DataFram = Data Table
+#DataFrame = Data Table
+import pandas as pd #importing pandas library
+import matplotlib.pyplot as plt # matplotlib is a plotting library
 
-import pandas as pd
 data = pd.read_csv('big_students.csv')
 
 data_cleaned = data.dropna()  # remove rows with null values
 data_cleaned = data.drop_duplicates() # remove duplicate rows
 print(data.isnull().sum()) # we know how many null values are in each column
 
+print(data.describe())  # gives us a statistical summary of the dataset
 
+# Plot the distribution of age
+plt.hist(data['age'], bins=10, color='blue', edgecolor='black') # Create a histogram of the 'age' column
+plt.title('Distribution of age') #Title of the plot
+plt.xlabel('age') #X-axis label
+plt.ylabel('Number of Students') #Y-axis label
+plt.show() #show the plot
 
+plt.hist(data['age'], bins=10, color='purple', edgecolor='red')
+plt.title('Distribtion of age')
+plt.xlabel('age')
+plt.ylabel('Number of Students')
 
 
 # print(len(data)) to know how many rows are in the dataset
